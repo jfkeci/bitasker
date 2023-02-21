f0e9d4995a57a939833c8322b0beeb292bf935fc
jfkeci <ja.lip132@gmail.com>
2023-02-21 16:03:13 +0100

    feat: add config and logger module to the app to be available globally - add httpexceptionfilter - add app controller for healthcheck endpoint - register router config

--- nestjs-server/src/app.module.ts
+++ nestjs-server/src/app.module.ts
@@ -1,10 +1,41 @@
 import { Module } from '@nestjs/common';
+import { WinstonModule } from 'nest-winston';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { envConfig } from './utils/config/env.config';
+import { APP_FILTER, RouterModule } from '@nestjs/core';
+import { AuthModule } from './resources/auth/auth.module';
+import { loggerConfig } from './utils/config/logger.config';
+import { routerConfig } from './utils/config/router.config';
+import { PrismaModule } from './utils/prisma/prisma.module';
+import { UsersModule } from './resources/users/users.module';
+import { HttpExceptionFilter } from './utils/filters/http-exception.filter';
 
 @Module({
-  imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    WinstonModule.forRoot(
+      loggerConfig(process.env.LOG_LEVEL, process.env.NODE_ENV, {
+        frequency: process.env.LOG_FREQUENCY,
+        maxFiles: process.env.LOG_MAX_FILES,
+        maxSize: process.env.LOG_MAX_FILE_SIZE
+      })
+    ),
+    RouterModule.register(routerConfig),
+    ConfigModule.forRoot(envConfig()),
+    PrismaModule,
+    UsersModule,
+    AuthModule
+  ],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter
+    }
+    /* {
+      provide: APP_GUARD,
+      useClass: AuthGuard
+    } */
+  ]
 })
 export class AppModule {}
