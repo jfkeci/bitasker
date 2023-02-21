684cdeea6f049a803776843b2c007518b00e6f16
jfkeci <ja.lip132@gmail.com>
2023-02-21 15:04:54 +0100

    chore: setup eslint and prettier - add eslint-plugin-tsdoc package - add winston logger packages

--- nestjs-server/.eslintrc.js
+++ nestjs-server/.eslintrc.js
@@ -1,24 +1,22 @@
 module.exports = {
+  root: true,
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    project: './tsconfig.json',
+    ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint', 'prettier', 'eslint-plugin-tsdoc'],
   extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
   ],
-  root: true,
-  env: {
-    node: true,
-    jest: true,
-  },
-  ignorePatterns: ['.eslintrc.js'],
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    'no-console': 'error',
+    'prettier/prettier': 'error',
+    'no-useless-escape': 0,
+    'tsdoc/syntax': 'warn',
   },
 };
