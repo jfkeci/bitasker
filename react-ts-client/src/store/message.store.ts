5036c8b6007f0471f8202dba0dec0ceca70f8932
jfkeci <ja.lip132@gmail.com>
2023-02-24 10:35:49 +0100

    chore: message store and app message related components

--- react-ts-client/src/store/message.store.ts
+++ react-ts-client/src/store/message.store.ts
@@ -1,20 +1,49 @@
-import { create } from "zustand";
+import { debounce } from 'lodash';
+import { create } from 'zustand';
 
-export interface MessageStore {
-  message: string | null;
-  type: "error" | "success" | "warning";
-  isMessageVisible: boolean;
-  setMessage: (message: string) => void;
-  removeMessage: () => void;
+interface NewMessage {
+  title: string;
+  body?: string | null;
+  type?: 'error' | 'success' | 'warning';
 }
 
-const useMessageStore = create<MessageStore>((set, get) => ({
-  message: null,
-  type: "success",
-  isMessageVisible: false,
+export interface AppMessage {
+  id: number;
+  title: string;
+  body?: string | null;
+  type?: 'error' | 'success' | 'warning';
+}
 
-  setMessage: (message: string) => set((state) => ({ message })),
-  removeMessage: () => set((state) => ({ message: null })),
+interface MessageStore {
+  messages: AppMessage[];
+  addMessage: (message: NewMessage) => void;
+  removeMessage: (id: number) => void;
+  clearMessages: () => void;
+}
+
+const useMessageStore = create<MessageStore>((set) => ({
+  messages: [],
+  addMessage: (message: NewMessage) => {
+    set((state) => {
+      const id = state.messages.length
+        ? Math.max(...(<number[]>state.messages.map((m) => m.id))) + 1
+        : 1;
+
+      debounce(() => {
+        state.removeMessage(id);
+      }, 500);
+
+      return {
+        messages: [
+          ...state.messages,
+          { id, ...message, type: message.type ?? 'success' },
+        ],
+      };
+    });
+  },
+  removeMessage: (id: number) =>
+    set((state) => ({ messages: state.messages.filter((m) => m.id !== id) })),
+  clearMessages: () => set((state) => ({ messages: [] })),
 }));
 
 export default useMessageStore;
