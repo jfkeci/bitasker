7480a84bd9fad94dc8267d17d0a7e5d82cad2a4d
jfkeci <ja.lip132@gmail.com>
2023-02-27 08:51:12 +0100

    feat: logout view - add routes to grid view holder

--- react-ts-client/src/components/core/grid-view-holder.component.tsx
+++ react-ts-client/src/components/core/grid-view-holder.component.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import UserView from '../../views/user.view';
 import LoginView from '../../views/login.view';
+import LogoutView from '../../views/logout-view';
 import WelcomeView from '../../views/welcome.view';
+import EncoderView from '../../views/encoder.view';
 import RegisterView from '../../views/register.view';
 import NotFoundView from '../../views/not-found.view';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
@@ -13,11 +15,13 @@ export default function GridViewHolder() {
       <div className="col-span-6 p-4">
         <Router>
           <Routes>
-            <Route path="/" element={<WelcomeView />} />
-            <Route path="/profile" element={<UserView />} />
             <Route path="/register" element={<RegisterView />} />
+            <Route path="/encoder" element={<EncoderView />} />
+            <Route path="/logout" element={<LogoutView />} />
+            <Route path="/profile" element={<UserView />} />
             <Route path="/login" element={<LoginView />} />
             <Route path="*" element={<NotFoundView />} />
+            <Route path="/" element={<WelcomeView />} />
           </Routes>
         </Router>
       </div>
