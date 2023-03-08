5811622b1dac38a929949fde2dd9c4d4dfa2ac0b
jfkeci <ja.lip132@gmail.com>
2023-03-08 21:12:53 +0100

    chore: update message

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,6 +9,6 @@ export class AppController {
     description: 'Returns "String encoder Backend" if server is running'
   })
   healthcheck() {
-    return 'This is aa ';
+    return 'This is aaa ';
   }
 }
